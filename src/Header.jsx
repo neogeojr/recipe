ad439d547c3346ad29b0b8fa1dac7bc96a68e9ed
George S <141447199+neogeojr@users.noreply.github.com>
2026-03-31 16:17:29 -0700

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={reactLogo} />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
